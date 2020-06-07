e0aa80dd73d8243d64716fbcc21733a03df927ea
unknown <jaya.wagaskar29@gmail.com>
2020-06-07 14:03:56 +0530

    added data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
---insert into user_details (lastName,firstName,phoneNumber,password,email,category) values ('wagaskar','jaya',9935445566,'jaya','jaya@gmail.com','fulfiller');
+insert into user_details (lastName,firstName,phoneNumber,password,email,category) values ('wagaskar','jaya',9935445566,'jaya','jaya@gmail.com','fulfiller');
 
 --insert into user_details (lastName,firstName,phoneNumber,password,email,category) values ('abc','maya',9933445166,'jaya','jack@gmail.com','supplier');
 
